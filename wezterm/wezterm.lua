e6d52c69bc9c3466fd91fa210b25e1f83189dc10
medevdk <12559925+medevdk@users.noreply.github.com>
2024-06-10 12:15:04 +0800

    font size and window decoration depends on os

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,14 +7,27 @@ wezterm.on('gui-startup', function()
   window:gui_window():maximize()
 end)
 
+-- local is_linux = function()
+--   return wezterm.target_triple:find("linux") ~= nil
+-- end
+
+local is_darwin = function()
+  return wezterm.target_triple:find("darwin") ~= nil
+end
 
 return {
+
   default_prog = { '/bin/zsh', '-l' },
+
   -- window_decorations = 'RESIZE',
+  window_decorations = is_darwin() and 'RESIZE' or 'TITLE | RESIZE',
 
   color_scheme = 'Hardcore',
+
   font = wezterm.font('JetBrainsMono Nerd Font'),
-  font_size = 11,
+  -- font size 14 on darwin and 11 on other
+  font_size = is_darwin() and 14 or 11,
+
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
 
@@ -28,7 +41,7 @@ return {
   leader = {
     key = 'a',
     mods = 'CTRL',
-    timeout_milliseconds = 1000
+    timeout_milliseconds = 2000
   },
 
   keys = {
@@ -38,12 +51,12 @@ return {
     { key = 'j', mods = 'LEADER',       action = act.ActivatePaneDirection 'Down', },
     { key = 'k', mods = 'LEADER',       action = act.ActivatePaneDirection 'Up', },
     { key = 'l', mods = 'LEADER',       action = act.ActivatePaneDirection 'Right', },
-    { key = 'h', mods = 'LEADER|SHIFT', action = act({AdjustPaneSize ={'Left',5}}) },
-    { key = 'j', mods = 'LEADER|SHIFT', action = act({AdjustPaneSize ={'Down',5}}) },
-    { key = 'k', mods = 'LEADER|SHIFT', action = act({AdjustPaneSize ={'Up',5}}) },
-    { key = 'l', mods = 'LEADER|SHIFT', action = act({AdjustPaneSize ={'Right',5}}) },
-    {key = 'c', mods='CTRL|SHIFT', action = act({CopyTo = "Clipboard"})},
-    {key = 'v', mods = 'CTRL|SHIFT', action = act ({PasteFrom = "Clipboard"})},
+    { key = 'h', mods = 'LEADER|SHIFT', action = act({ AdjustPaneSize = { 'Left', 5 } }) },
+    { key = 'j', mods = 'LEADER|SHIFT', action = act({ AdjustPaneSize = { 'Down', 5 } }) },
+    { key = 'k', mods = 'LEADER|SHIFT', action = act({ AdjustPaneSize = { 'Up', 5 } }) },
+    { key = 'l', mods = 'LEADER|SHIFT', action = act({ AdjustPaneSize = { 'Right', 5 } }) },
+    { key = 'c', mods = 'CTRL|SHIFT',   action = act({ CopyTo = "Clipboard" }) },
+    { key = 'v', mods = 'CTRL|SHIFT',   action = act({ PasteFrom = "Clipboard" }) },
 
   },
 }
