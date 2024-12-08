1dcb3e23e054f841b7c011d25d492c753e082160
medevdk <12559925+medevdk@users.noreply.github.com>
2024-12-08 11:49:07 +0800

    lazygit from snacks

---
+++ nvim/lua/devdk/plugins/snacks.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    notifier = { enabled = true },
+    lazygit = { enabled = true },
+  },
+  keys = {
+    { "<leader>lg", function() Snacks.lazygit() end, desc = "Lazygit " },
+  },
+}
