fc9b25f5c46be53e8694af05b188259e4d4a4a02
medevdk <12559925+medevdk@users.noreply.github.com>
2024-12-16 12:59:04 +0800

    change keymap conform

--- nvim/lua/devdk/plugins/formatting.lua
+++ nvim/lua/devdk/plugins/formatting.lua
@@ -23,7 +23,7 @@ return {
       },
     })
 
-    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+    vim.keymap.set({ "n", "v" }, "<leader>fp", function()
       conform.format({
         lsp_fallback = true,
         async = false,
