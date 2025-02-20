c47e9a8ecf0d83d874a405d2a7a6ee9616b5f6d9
medevdk <12559925+medevdk@users.noreply.github.com>
2025-02-20 11:51:43 +0800

    add regex

--- nvim/lua/devdk/plugins/treesitter.lua
+++ nvim/lua/devdk/plugins/treesitter.lua
@@ -1,43 +1,70 @@
 return {
-  -- require("lazy").setup({
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    dependencies = { "windwp/nvim-ts-autotag" },
-    event = { "BufReadPre", "BufNewFile" },
-
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-        auto_install = true,
-        ensure_installed = {
-          "lua",
-          "vim",
-          "markdown",
-          "markdown_inline",
-          "json",
-          "html",
-          "css",
-          "vimdoc",
-          "go",
-          "gomod",
-          "gowork",
-          "gosum",
-          "c",
-          "cpp"
-        },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-        autotag = { enable = true, },
-        autopairs = { enable = true },
-        context_commentstring = {
-          enable = true,
-          enable_autocmd = false,
-        },
-      })
-    end,
-  },
-  -- }),
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		dependencies = { "windwp/nvim-ts-autotag" },
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			ensure_installed = {
+				"lua",
+				"vim",
+				"markdown",
+				"markdown_inline",
+				"json",
+				"html",
+				"css",
+				"vimdoc",
+				"go",
+				"gomod",
+				"gowork",
+				"gosum",
+				"c",
+				"cpp",
+				"regex",
+			},
+		},
+	},
 }
+-- return {
+--   -- require("lazy").setup({
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     build = ":TSUpdate",
+--     dependencies = { "windwp/nvim-ts-autotag" },
+--     event = { "BufReadPre", "BufNewFile" },
+--
+--     config = function()
+--       local configs = require("nvim-treesitter.configs")
+--
+--       configs.setup({
+--         auto_install = true,
+--         ensure_installed = {
+--           "lua",
+--           "vim",
+--           "markdown",
+--           "markdown_inline",
+--           "json",
+--           "html",
+--           "css",
+--           "vimdoc",
+--           "go",
+--           "gomod",
+--           "gowork",
+--           "gosum",
+--           "c",
+--           "cpp"
+--         },
+--         sync_install = false,
+--         highlight = { enable = true },
+--         indent = { enable = true },
+--         autotag = { enable = true, },
+--         autopairs = { enable = true },
+--         context_commentstring = {
+--           enable = true,
+--           enable_autocmd = false,
+--         },
+--       })
+--     end,
+--   },
+--   -- }),
+-- }
