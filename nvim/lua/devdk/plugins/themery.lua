c028832d6eca1b21e868a2f51cf8e80f185fe7cf
medevdk <12559925+medevdk@users.noreply.github.com>
2025-03-05 09:48:52 +0800

    add theme, changed title

--- nvim/lua/devdk/plugins/themery.lua
+++ nvim/lua/devdk/plugins/themery.lua
@@ -1,50 +1,60 @@
 return {
 
-  { "folke/tokyonight.nvim" },
-  { "catppuccin/nvim" },
-  { "rebelot/kanagawa.nvim" },
-  { "rose-pine/neovim" },
+	{ "folke/tokyonight.nvim" },
+	{ "catppuccin/nvim" },
+	{ "rebelot/kanagawa.nvim" },
+	{ "rose-pine/neovim" },
 
-  {
+	{
 
-    "zaldih/themery.nvim",
+		"zaldih/themery.nvim",
 
-    config = function()
-      require("themery").setup({
-        themes = {
-          { name = "",        colorscheme = "catppuccin-frappe" },
-          { name = "--night", colorscheme = "catppuccin-frappe" },
-          {
-            name = "TokyoNight ",
-            colorscheme = "tokyonight-night"
-          },
-          {
-            name = "Kanagawa ",
-            colorscheme = "kanagawa-wave"
-          },
-          {
-            name = "Catppuccin ",
-            colorscheme = "catppuccin-mocha"
-          },
-          {
-            name = "Rose Pine ",
-            colorscheme = "rose-pine-main"
-          },
+		config = function()
+			require("themery").setup({
+				themes = {
+					{ name = "", colorscheme = "catppuccin-frappe" },
+					{ name = "--night", colorscheme = "catppuccin-frappe" },
+					{
+						name = "TokyoNight ",
+						colorscheme = "tokyonight-night",
+					},
+					{
+						name = "Kanagawa Wave",
+						colorscheme = "kanagawa-wave",
+					},
 
-          { name = "",      colorscheme = "tokyonight-storm" },
-          { name = "--day", colorscheme = "tokyonight-storm" },
+					{
+						name = "Kanagawa Dragon",
+						colorscheme = "kanagawa-dragon",
+					},
+					{
+						name = "Catppuccin Mocha",
+						colorscheme = "catppuccin-mocha",
+					},
+					{
+						name = "Rose Pine ",
+						colorscheme = "rose-pine-main",
+					},
 
-          {
-            name = "Catppuccin ",
-            colorscheme = "catppuccin-latte"
-          },
+					{ name = "", colorscheme = "tokyonight-storm" },
+					{ name = "--day", colorscheme = "tokyonight-storm" },
 
-          {
-            name = "TokyoNight ",
-            colorscheme = "tokyonight-day"
-          },
-        },
-      })
-    end
-  }
+					{
+						name = "Catppuccin ",
+						colorscheme = "catppuccin-latte",
+					},
+
+					{
+						name = "TokyoNight ",
+						colorscheme = "tokyonight-day",
+					},
+
+					{
+						name = "Kamagawe ",
+						colorscheme = "kanagawa-lotus",
+					},
+				},
+			})
+		end,
+	},
 }
