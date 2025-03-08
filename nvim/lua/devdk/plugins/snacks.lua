fd7523ccf31efa9cf79613cc6264211e686a8915
medevdk <12559925+medevdk@users.noreply.github.com>
2025-03-08 10:01:46 +0800

    add snacks dashboard

--- nvim/lua/devdk/plugins/snacks.lua
+++ nvim/lua/devdk/plugins/snacks.lua
@@ -1,12 +1,19 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  opts = {
-    notifier = { enabled = true },
-    lazygit = { enabled = true },
-  },
-  keys = {
-    { "<leader>lg", function() Snacks.lazygit() end, desc = "Lazygit " },
-  },
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		notifier = { enabled = true },
+		lazygit = { enabled = true },
+		dashboard = { enabled = true },
+	},
+	keys = {
+		{
+			"<leader>lg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit ",
+		},
+	},
 }
