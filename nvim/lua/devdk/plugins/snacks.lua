bd6066959c9128a12fa77b20b35558426724b934
medevdk <12559925+medevdk@users.noreply.github.com>
2025-06-27 05:18:15 +0800

    remove snacks start screen

--- nvim/lua/devdk/plugins/snacks.lua
+++ nvim/lua/devdk/plugins/snacks.lua
@@ -1,19 +1,19 @@
 return {
-	"folke/snacks.nvim",
-	priority = 1000,
-	lazy = false,
-	opts = {
-		notifier = { enabled = true },
-		lazygit = { enabled = true },
-		dashboard = { enabled = true },
-	},
-	keys = {
-		{
-			"<leader>lg",
-			function()
-				Snacks.lazygit()
-			end,
-			desc = "Lazygit ",
-		},
-	},
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    notifier = { enabled = true },
+    lazygit = { enabled = true },
+    dashboard = { enabled = false },
+  },
+  keys = {
+    {
+      "<leader>lg",
+      function()
+        Snacks.lazygit()
+      end,
+      desc = "Lazygit ",
+    },
+  },
 }
